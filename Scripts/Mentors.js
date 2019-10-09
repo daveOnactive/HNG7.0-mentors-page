a3e37a4ea7fd616eaa280b5e89e72d325d4ef99b
Ego <47955681+egocious@users.noreply.github.com>
2019-10-09 22:15:25 +0100

    Update on file

--- Scripts/Mentors.js
+++ Scripts/Mentors.js
@@ -1,1 +1,231 @@
 
+mentorsArr = [
+ {
+   name: "Oluwatoni Atunrase",
+   stack: "Web Developer",
+   imgUrl: "./images/imagename.jpg"
+ },
+ {
+    name: "Patrick Aziken",
+    stack: "Software Tester",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Stephen Azubuike",
+    stack: "UI/UX Designer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Everitus Olumese",
+    stack: "Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Osumgha Chiamaka",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Aboagye",
+    stack: "Web Developer",
+    imgUrl:"./images/imagename.jpg"
+  },
+  {
+    name: "John Ademoye",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Adeyefa Oluwatoba",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Adolphus Chris",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Ogundiji Bolade",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Ajayi Jeremiah",
+    stack: "Backend Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Albert Oboh",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Amujo Oluwasomido",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Chigozie",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Kev Chike",
+    stack: "Software Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Chinedu Ebinim",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Chukwuemeka Iheonye",
+    stack: "Product Designer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Somtozech",
+    stack: "Web Developer",
+    imgUrl:"./images/imagename.jpg"
+  },
+  {
+    name: "Cole Solomon",
+    stack: "Product Designer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Motunrayo Da-costa",
+    stack: "Frontend Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Maaruf Dauda",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Romina Hadid",
+    stack: "Marketing Strategist",
+    imgUrl: "./images/imagename.jpg"
+  },
+   {
+    name: "Alexander Smith",
+    stack: "UI/UX Designer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "John Doe",
+    stack: "Founder and CEO",
+    imgUrl: "./images/imagename.jpg"
+  },
+   {
+    name: "Ryan Thompson",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Romina Hadid",
+    stack: "Marketing Strategist",
+    imgUrl:"./images/imagename.jpg"
+  },
+   {
+    name: "Alexander Smith",
+    stack: "UI/UX Designer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "John Doe",
+    stack: "Founder and CEO",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Ryan Thompson",
+    stack: "Web Developer",
+    imgUrl:"./images/imagename.jpg" 
+  },
+  {
+    name: "Romina Hadid",
+    stack: "Marketing Strategist",
+    imgUrl: "./images/imagename.jpg"
+  },
+   {
+    name: "Alexander Smith",
+    stack: "UI/UX Designer",
+    imgUrl:"./images/imagename.jpg"
+  },
+  {
+    name: "John Doe",
+    stack: "Founder and CEO",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Ryan Thompson",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Romina Hadid",
+    stack: "Marketing Strategist",
+    imgUrl: "./images/imagename.jpg"
+  },
+   {
+    name: "Alexander Smith",
+    stack: "UI/UX Designer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "John Doe",
+    stack: "Founder and CEO",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Ryan Thompson",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Romina Hadid",
+    stack: "Marketing Strategist",
+    imgUrl: "./images/imagename.jpg"
+  },
+   {
+    name: "Alexander Smith",
+    stack: "UI/UX Designer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "John Doe",
+    stack: "Founder and CEO",
+    imgUrl: "./images/imagename.jpg" 
+  },
+  {
+    name: "Ryan Thompson",
+    stack: "Web Developer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "Romina Hadid",
+    stack: "Marketing Strategist",
+    imgUrl: "./images/imagename.jpg"
+  },
+   {
+    name: "Alexander Smith",
+    stack: "UI/UX Designer",
+    imgUrl: "./images/imagename.jpg"
+  },
+  {
+    name: "John Doe",
+    stack: "Founder and CEO",
+    imgUrl: "./images/imagename.jpg"
+  },
+]
+
+
+
+
+
+
+
+
